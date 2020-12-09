5730219ddcb1638aa085ebbda8aa0f84fb550415
xQbsonKEKW <62252139+xQbsonKEKW@users.noreply.github.com>
2020-12-09 20:50:38 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+using namespace std;
+
+vector< char > foo(list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+	vector<char> vec;
+
+	for (auto rit = people.rbegin(); rit != people.rend(); rit++) 
+	{
+		rit->birthday();
+
+        if (rit->isMonster())
+		{
+			vec.push_back('n');
+		}
+		else if (!(rit->isMonster()))
+		{
+			vec.push_back('y');
+		}
+	}
+	return {vec};
 }
